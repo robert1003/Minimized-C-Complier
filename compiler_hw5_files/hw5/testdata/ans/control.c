5b41a3681d1f2b2517e1d53ae7b96ed22151d9ba
robert1003 <b07902047@ntu.edu.tw>
2021-01-01 03:50:42 +0800

    add and

---
+++ compiler_hw5_files/hw5/testdata/ans/control.c
@@ -0,0 +1,21 @@
+make: 'parser' is up to date.
+Parsing completed. No errors found.
+correct
+correct
+correct
+correct
+correct
+correct
+correct
+correct
+correct: 0
+correct: 1
+correct: 2
+correct: 3
+correct: 4
+correct: 5
+correct: 6
+correct: 7
+correct: 8
+correct: 9
+correct
